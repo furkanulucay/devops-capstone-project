6829773a62aff836ac1e8119220dff8580bd932e
Furkan Uluçay <131382390+furkanulucay@users.noreply.github.com>
2025-07-29 14:52:05 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -62,7 +62,7 @@ def create_accounts():
 ######################################################################
 
 
-@app.route("/accounts", methods = ["GET"])
+@app.route("/accounts", methods=["GET"])
 def list_accounts():
     app.logger.info("Request to list Accounts")
 
@@ -94,7 +94,7 @@ def read_account(account_id):
 ######################################################################
 
 
-@app.route("/accounts/<int:account_id>", methods = ["PUT"])
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
@@ -112,7 +112,7 @@ def update_account(account_id):
 ######################################################################
 
 
-@app.route("/accounts/<int:account_id>", methods = ["DELETE"])
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)

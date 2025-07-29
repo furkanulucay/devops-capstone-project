7503cb31c85929730677ff3e67b80fe99d4a7631
Furkan Uluçay <131382390+furkanulucay@users.noreply.github.com>
2025-07-29 14:57:20 +0300

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -86,7 +86,7 @@ def read_account(account_id):
     if(not account):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-    return account.serialize(), status.HTTP_200_OK    
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
